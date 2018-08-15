957ac9aebb28e2f07f1b75b0e45579c282560d91
ttay <ttay@umich.edu>
2018-08-15 15:07:27 +0800

    Added files to gitignore

--- upload_photo.py
+++ upload_photo.py
@@ -8,13 +8,11 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("login.ini")
-
 username = config["LOGIN"]["username"]
 passwd = config["LOGIN"]["password"]
 InstagramAPI = InstagramAPI(username, passwd)
-if (InstagramAPI.login()):
-    InstagramAPI.getSelfUserFeed()  # get self user feed
-    print(InstagramAPI.LastJson)  # print last response JSON
-    print("Login success!")
-else:
-    print("Can't login!")
+InstagramAPI.login()  # login
+
+photo_path = '/home/thomas/Pictures/girl1.jpg'
+caption = "This is too real #deep"
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
